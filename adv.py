3f73c99d52752f4bd0d20cd3305253cf9d089ef6
RSibaja88 <54650378+RSibaja88@users.noreply.github.com>
2020-09-26 03:54:34 -0400

    MVP 1000

--- adv.py
+++ adv.py
@@ -2,6 +2,7 @@ from room import Room
 from player import Player
 from world import World
 
+
 import random
 from ast import literal_eval
 
@@ -17,7 +18,7 @@ world = World()
 map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -25,10 +26,61 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+
+"""
+Plan:
+move player into starting room
+find all possible exits for room
+move in a direction and repeat this process
+Plan to keep track of visited rooms/paths
+Set direction for reverse as well
+
+Based on this problem: storage space isnt an issue, getting this task accomplished quickly and as efficiently as possible is the main goal. For this, DFS(t) is most likely the ideal solution. 
+
+
+MY CODE STARTING HERE!!!
+
+"""
+
+reverse_move = {'n': 's', 's': 'n', 'w': 'e', 'e': 'w'}
+
+
+def dftr_maze(current_room, visited=None):
+    # list directions while moving
+    directions = []
+    # creating a set to hold visited rooms if visited is none.
+    if visited == None:
+        visited = set()
+
+    # finding exits for current_room using player from player and get_exits from room
+    for move in player.current_room.get_exits():
+        # making moves using travel from player
+        player.travel(move)
+
+        # reverse_move if already visited to find new path
+        if player.current_room in visited:
+            player.travel(reverse_move[move])
+        # if its a new room, do the following
+        else:
+            # add to visited stack
+            visited.add(player.current_room)
+            # adding this move to 'directions'
+            directions.append(move)
+            # recursive : repeating loop and adding directions (sidenote, '\' is an escape character in Python.)
+            directions = directions + \
+                dftr_maze(player.current_room, visited)
+            # go_back to previous room
+            player.travel(reverse_move[move])
+            # appending this move to 'directions'
+            directions.append(reverse_move[move])
+
+    return directions
+
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
-
+traversal_path = dftr_maze(player.current_room)
+print(dftr_maze)
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -41,11 +93,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
